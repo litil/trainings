79d65a5cf6cedacc9def54ca1db5829c97eb8530
Guillaume Lambert <glambert@bluedme.com>
2018-02-07 18:44:13 +0100

    Added linter script

--- NodeJS/ExpressApp/index.js
+++ NodeJS/ExpressApp/index.js
@@ -1,10 +1,10 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function(req, res) {
-    res.send('Hello World!')
+app.get('/', function (req, res) {
+  res.send('Hello World!')
 })
 
-app.listen(8000, function() {
-    console.log('Example app listening on port 8000!')
+app.listen(8000, function () {
+  console.log('Example app listening on port 8000!')
 })
